cbc14d6ba37db3eb8a1afd332554d7bc271d8073
KrRishabh <kr.rishabh72@gmail.com>
2021-05-19 18:45:23 +0530

    MVVM basic structure written

---
+++ WPFWeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFWeatherApp.Model;
+
+namespace WPFWeatherApp.ViewModel
+{
+    public class WeatherVM: INotifyPropertyChanged
+    {
+
+
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentCondition currentConditions;
+
+        public CurrentCondition CurrentConditions
+        {
+            get { return currentConditions; }
+            set { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
